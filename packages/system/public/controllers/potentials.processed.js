f0adf577733450eea3fa8d4b4eb972b345c6c91d
nharlow89 <nharlow89@gmail.com>
2014-08-13 15:58:02 -0700

    renamed site, made it easier to view comments and messages on the Processed Users view

--- packages/system/public/controllers/potentials.processed.js
+++ packages/system/public/controllers/potentials.processed.js
@@ -1,7 +1,16 @@
 'use strict';
 
-angular.module('mean.system').controller('ProcessedPotentialsController', ['$scope', '$rootScope', '$window', 'Global', '$http', 'PotentialUsers', '$state',
-    function($scope, $rootScope, $window, Global, $http, PotentialUsers, $state) {
+var ModalInstanceCtrl = function ($scope, $modalInstance, username, message) {
+    $scope.username = username;
+    $scope.message = message ? message : '(No message and/or comments)';
+
+    $scope.ok = function () {
+        $modalInstance.close();
+    };
+};
+
+angular.module('mean.system').controller('ProcessedPotentialsController', ['$scope', '$rootScope', '$window', 'Global', '$http', 'PotentialUsers', '$state', '$modal',
+    function($scope, $rootScope, $window, Global, $http, PotentialUsers, $state, $modal) {
         $scope.global = Global;
 
         PotentialUsers.getProcessedPotentialUsers().success(function ( response ) {
@@ -16,6 +25,23 @@ angular.module('mean.system').controller('ProcessedPotentialsController', ['$sco
         	}).error( function () {
         		pUser.error = true;
         	});
+        };
+
+        $scope.openModal = function ( username, message ) {
+
+            $modal.open({
+                templateUrl: 'messageModal.html',
+                controller: ModalInstanceCtrl,
+                resolve: {
+                    username: function () {
+                        return username;
+                    },
+                    message: function() {
+                        return message;
+                    }
+                }
+            });
+
         };
     }
 ]);
